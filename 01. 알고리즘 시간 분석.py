fc38ab9fda9936d239d68afd8b563fb0f894d941
94jjiisu <52755643+94jjiisu@users.noreply.github.com>
2021-05-17 23:27:26 +0900

    Update 01. 알고리즘 시간 분석.py

--- 01. 알고리즘 시간 분석.py
+++ 01. 알고리즘 시간 분석.py
@@ -63,3 +63,60 @@ def prefix_average3(S):
     A[j] = total / (j+1) # compute average based on current sum
   return A
 
+
+# O(n^3) 시간 알고리즘(Nested Loop)
+# 세 종류의 집합 분리(Three-Way Set Disjointness)
+
+def disjoint1(A, B, C):
+  """Return True if there is no element common to all three lists."""
+  for a in A:
+    for b in B:
+      for c in C:
+        if a == b == c:
+          return False # we found a common value
+  return True
+
+
+# O(n^2) 시간 알고리즘
+# 세 종류의 집합 분리(Three-Way Set Disjointness)
+
+def disjoint2(A, B, C):
+  """Return True if there is no element common to all three lists."""
+  for a in A:
+    for b in B:
+      if a == b: # only check C if we found match from A and B
+        for c in C:
+          if a == c # (and thus a == b == c)
+            return False # we found a common value
+  return True # if we reach this, sets are disjoint
+
+
+# O(n^2) 시간 알고리즘
+# 집합 중복 체크(Element Uniqueness)
+
+def unique1(S):
+  """Return True if there are no duplicate elements in sequence S."""
+  for j in range(len(S)):
+    for k in range(j+1, len(S)):
+      if S[j] == S[k]:
+        return False # found duplicate pair
+  return True # if we reach this, elements were unique
+
+
+# O(nlogn) 시간 알고리즘
+# 집합 중복 체크(Element Uniqueness)
+
+def unique2(S):
+  """Return True if there are no duplicate elements in sequence S."""
+  temp = sorted(S) # create a sorted copy of S
+  for j in range(1, len(temp)):
+    if S[j-1] == S[j]:
+      return False # found duplicate pair
+  return True # if we reach this, elements were unique
+
+
+# O(2^n)시간 알고리즘(피보나치 수열)
+
+def fibonacci(k):
+  if k <= 1: return k
+  return fibonacci(k-1)+fibonacci(k-2)
