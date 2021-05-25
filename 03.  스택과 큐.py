5968c5c7bf303c4e258fdefc2a2d8b0cf8c758aa
94jjiisu <52755643+94jjiisu@users.noreply.github.com>
2021-05-25 23:43:32 +0900

    Update 03.  스택과 큐.py

--- 03.  스택과 큐.py
+++ 03.  스택과 큐.py
@@ -68,3 +68,37 @@ class ArrayStack:
       raise Empty('Stack is empty')
     return self._data.pop() # remove last item from list
   
+  
+  
+ 
+
+# 예시1 - 문서 뒤집기
+
+# 스택 생성
+def reverse_file(filename):
+  """Overwrite given file with its contents line-by-line reversed."""
+  S = ArrayStack()
+# 스택을 생성해서 S로 둔다.
+
+# 스택에 라인 별로 문서 넣기
+  original = open(filename)
+  for line in original:
+    S.push(line.rstrip('\n')) # we will re-insert newlines when writing
+  original.close()
+#파일에서 문서를 읽고 문서의 각 라인을 newline을 제거해서 스택에 넣는다.
+
+
+# 스택에서 라인을 꺼내서 거꾸로 된 문서 생성
+# now we overwrite with contents in LIFO order
+  output = open(filename, 'w') # reopening file overwrites original
+  while not S.is_empty():
+    output.write(S.pop() + '\n') # re-insert newline characters
+  output.close()
+# 스택에서 꺼낼 때는 newline을 추가해서 문서 파일에 쓴다.
+
+
+
+# 예시2 - 수식에서 괄호 문법 체크
+
+ 
+  
