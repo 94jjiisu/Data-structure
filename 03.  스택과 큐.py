951025336da1e12f61c6fc35cdc7bb5dbdab5536
94jjiisu <52755643+94jjiisu@users.noreply.github.com>
2021-05-26 22:20:14 +0900

    Update 03.  스택과 큐.py

--- 03.  스택과 큐.py
+++ 03.  스택과 큐.py
@@ -100,5 +100,39 @@ def reverse_file(filename):
 
 # 예시2 - 수식에서 괄호 문법 체크
 
- 
+def is_matched(expr):
+  """Return True if all delimiters are properly match; False otherwise."""
+  lefty = '({[' # opening delimiters
+  righty = ')}]' # respective closing delims
+  S = ArrayStack()
+  for c in expr:
+    if c in lefty:
+      S.push(c) # push left delimiter on stack
+    elif c in righty:
+      if S.is_empty():
+        return False # nothing to match with
+      if righty.index(c) != lefty.index(S.pop()):
+        return False # mismatched
+  return S.is_empty() # were all symbols matched?
   
+
+# 예시3 - HTML 문법 체크
+
+def is_matched_html(raw):
+  """Return True if all HTML tags are properly match; False otherwise."""
+  S = ArrayStack()
+  j = raw.find('<') # find first '<' character (if any)
+  while j != -1:
+    k = raw.find('>', j+1) # find next '>' character
+    if k == -1:
+      return False # invalid tag
+    tag = raw[j+1:k] # strip away < >
+    if not tag.startswith('/'): # this is opening tag
+      S.push(tag)
+    else: # this is closing tag
+      if S.is_empty():
+        return False # nothing to match with
+      if tag[1:] != S.pop():
+        return False # mismatched delimiter
+    j = raw.find('<', k+1) # find next '<' character (if any)
+  return S.is_empty() # were all opening tags matched?
