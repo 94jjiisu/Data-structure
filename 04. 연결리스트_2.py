8d653177d0e68def9662418f3e752a6e37933195
94jjiisu <52755643+94jjiisu@users.noreply.github.com>
2021-06-04 23:39:34 +0900

    Update 04. 연결리스트_2.py

--- 04. 연결리스트_2.py
+++ 04. 연결리스트_2.py
@@ -1,7 +1,4 @@
 ### 포지션 리스트 ###
-
-class PositionalList(_DoublyLinkedBase):
-  """A sequential container of elements allowing positional access.""
   
 class Position:
   """An abstraction representing the location of a single element.
@@ -23,3 +20,86 @@ class Position:
   def __ne__(self, other):
     """Return True if other does not represent the same location."""
     return not (self == other) # opposite of __eq__
+
+
+class PositionalList(_DoublyLinkedBase):
+  """A sequential container of elements allowing positional access.""
+  
+  def _validate(self, p):
+    """Return position's node, or raise appropriate error if invalid."""
+    if not isinstance(p, self.Position):
+      raise TypeError('p must be proper Position type')
+    if p._container is not self:
+      raise ValueError('p does not belong to this container')
+    if p._node._next is None: # convention for deprecated nodes
+      raise ValueError('p is no longer valid')
+    return p._node
+  
+  def _make_position(self, node):
+    """Return Position instance for given node (or None if sentinel)."""
+    if node is self._header or node is self._trailer:
+      return None # boundary violation
+    else:
+      return self.Position(self, node) # legitimate position
+    
+  def first(self):
+    """Return the first Position in the list (or None if list is empty)."""
+    return self._make_position(self._header._next)
+  
+  def last(self):
+    """Return the last Position in the list (or None if list is empty)."""
+    return self._make_position(self._trailer._prev)
+  
+  def before(self, p):
+    """Return the Position just before Position p (or None if p is first)."""
+    node = self._validate(p)
+    return self._make_position(node._prev)
+  
+  def after(self, p):
+    """Return the Position just after Position p (or None if p is last)."""
+    node = self._validate(p)
+    return self._make_position(node._next)
+  
+  def __iter__(self):
+    """Generate a forward iteration of the elements of the list."""
+    cursor = self.first()
+    while cursor is not None:
+      yield cursor.element()
+      cursor = self.after(cursor)
+      
+  def _insert_between(self, e, predecessor, successor):
+    """Add element between existing nodes and return new Position."""
+    node = super()._insert_between(e, predecessor, successor)
+    return self._make_position(node)
+  
+  def add_first(self, e):
+    """Insert element e at the front of the list and return new Position."""
+    return self._insert_between(e, self._header, self._header._next)
+  
+  def add_last(self, e):
+    """Insert element e at the back of the list and return new Position."""
+    return self._insert_between(e, self._trailer._prev, self._trailer)
+  
+  def add_before(self, p, e):
+    """Insert element e into list before Position p and return new Position."""
+    original = self._validate(p)
+    return self._insert_between(e, original._prev, original)
+  
+  def add_after(self, p, e):
+    """Insert element e into list after Position p and return new Position."""
+    original = self._validate(p)
+    return self._insert_between(e, original, original._next)
+  
+  def delete(self, p):
+    """Remove and return the element at Position p."""
+    original = self._validate(p)
+    return self._delete_node(original) # inherited method returns element
+  
+  def replace(self, p, e):
+    """Replace the element at Position p with e.
+    Return the element formerly at Position p.
+    """
+    original = self._validate(p)
+    old_value = original._element # temporarily store old element
+    original._element = e # replace with new element
+    return old_value # return the old element value
