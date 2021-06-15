b02ff553edf166e24f8078b369de9c3b94d612b2
94jjiisu <52755643+94jjiisu@users.noreply.github.com>
2021-06-15 23:05:03 +0900

    Update 08. 탐색 트리_01.py

--- 08. 탐색 트리_01.py
+++ 08. 탐색 트리_01.py
@@ -301,6 +301,9 @@ class TreeMap(LinkedBinaryTree, MapBase):
     """Call to indicate that position p was recently accessed."""
     pass
 
+  
+### 확장된 노드 클래스 ###
+
 class RedBlackTreeMap(TreeMap):
   
   class _Node(TreeMap._Node):
@@ -312,3 +315,75 @@ class RedBlackTreeMap(TreeMap):
       
       
       
+      
+### AVL 트리 ###
+
+### AVLTreeMap 클래스 ###
+
+from .binary_search_tree import TreeMap
+
+class AVLTreeMap(TreeMap):
+  """Sorted map implementation using an AVL tree."""
+  
+
+  ## 노드 클래스 ##
+  class _Node(TreeMap._Node):
+    """Node class for AVL maintains height value for balancing.
+    We use convention that a "None" child has height 0, thus a leaf has height 1.
+    """
+    __slots__ = '_height' # additional data member to store height
+    
+    def __init__(self, element, parent=None, left=None, right=None):
+      super().__init__(element, parent, left, right)
+      self._height = 0 # will be recomputed during balancing
+      
+    def left_height(self):
+      return self._left._height if self._left is not None else 0
+    
+    def right_height(self):
+      return self._right._height if self._right is not None else 0
+    
+  ## AVLTreeMap 클래스 ###
+  
+  def _recompute_height(self, p):
+    p._node._height = 1 + max(p._node.left_height(), p._node.right_height())
+    
+  def _isbalanced(self, p):
+    return abs(p._node.left_height() - p._node.right_height()) <= 1
+  
+  def _tall_child(self, p, favorleft=False): # parameter controls tiebreaker
+    if p._node.left_height() + (1 if favorleft else 0) > p._node.right_height():
+      return self.left(p)
+    else:
+      return self.right(p)
+    
+  def _tall_grandchild(self, p):
+    child = self._tall_child(p)
+    # if child is on left, favor left grandchild; else favor right grandchild
+    alignment = (child == self.left(p))
+    return self._tall_child(child, alignment)
+  
+  def _rebalance(self, p):
+    while p is not None:
+      old_height = p._node._height # trivially 0 if new node
+      if not self._isbalanced(p): # imbalance detected!
+        # perform trinode restructuring, setting p to resulting root,
+        # and recompute new local heights after the restructuring
+        p = self._restructure(self._tall_grandchild(p))
+        self._recompute_height(self.left(p))
+        self._recompute_height(self.right(p))
+      self._recompute_height(p) # adjust for recent changes
+      if p._node._height == old_height: # has height changed?
+        p = None # no further changes needed
+      else:
+        p = self.parent(p) # repeat with parent
+        
+  def _rebalance_insert(self, p):
+    self._rebalance(p)
+    
+  def _rebalance_delete(self, p):
+    self._rebalance(p)
+    
+    
+    
+  
