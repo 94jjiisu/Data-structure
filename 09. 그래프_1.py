bca2700da750c0b3c50687d7ac8f120a5d313aae
94jjiisu <52755643+94jjiisu@users.noreply.github.com>
2021-06-18 23:57:19 +0900

    Create 09. 그래프_1.py

---
+++ 09. 그래프_1.py
@@ -0,0 +1,19 @@
+### Vertex 클래스 ###
+
+class Vertex:
+  """Lightweight vertex structure for a graph."""
+  __slots__ = '_element'
+  
+  def __init__(self, x):
+    """Do not call constructor directly. Use Graph's insert_vertex(x)."""
+    self._element = x
+    
+  def element(self):
+    """Return element associated with this vertex."""
+    return self._element
+  
+  def __hash__(self): # will allow vertex to be a map/set key
+    return hash(id(self))
+  
+  def __str__(self):
+    return str(self._element)
